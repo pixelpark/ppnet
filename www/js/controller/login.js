1462bd999a67d27c1193b7731f9620fe90ed0455
tobiasrotter <info@tobias-rotter.de>
2014-01-16 14:13:21 +0100

    FCTWO-194

--- www/js/controller/login.js
+++ www/js/controller/login.js
@@ -1,5 +1,13 @@
 function LoginController($scope){
-
+ 	$scope.db.changes({
+					  since:  'latest',
+					  continuous: true,
+					  include_docs: true,
+					  onChange:  function(change) {
+					  	 console.log('LoginController');
+					  }
+	});
+ 	
 
 
 }
---
+++ www/js/user.js
@@ -0,0 +1,35 @@
+function User($scope){	
+	/*
+     *  USER
+     */
+    //window.localStorage.clear();
+    console.log('init');
+	$scope.user = {vars:{}};
+	$scope.user.id = window.localStorage.getItem("user.id");
+	$scope.user.name = window.localStorage.getItem("user.name");
+	$scope.user.isLogedIn = function(){
+		return ($scope.user.id)?true:false;
+	};
+	
+	$scope.user.login = function(){
+		if($scope.user.name && $scope.user.id){
+			 window.localStorage.setItem("user.name", $scope.user.name);
+			 window.localStorage.setItem("user.id", $scope.user.id);
+			 window.location='./main.html';
+		}else{
+			
+		}
+	};
+	
+	$scope.user.logout = function(){
+		window.localStorage.clear();
+		window.location='./main.html';
+	};
+	
+	$scope.user.getName = function(){
+		return $scope.user.name;
+	};
+	$scope.user.getId = function(){
+		return $scope.user.id;
+	};
+}
