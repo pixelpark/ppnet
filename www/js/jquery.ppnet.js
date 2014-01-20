8e4a71474a03ce059a4a2e3fc50300b71c337174
PhilReinking <media@philippreinking.de>
2014-01-20 15:17:06 +0100

    console.log entfernt

--- www/js/jquery.ppnet.js
+++ www/js/jquery.ppnet.js
@@ -3,11 +3,9 @@ function stickyActionBar() {
 		if(direction === "down"){
 			jQuery('.app_action_wrapper').addClass('sticky');
 			jQuery('.postings').addClass('sticky');
-			console.log('Added Class');
 		} else {
 			jQuery('.app_action_wrapper').removeClass('sticky');
 			jQuery('.postings').removeClass('sticky');
-			console.log('Removed Class');
 		}
 	});
 	return true;
