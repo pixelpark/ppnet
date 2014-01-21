5ed68dc95985a90f1326254fbbb3b1a21d7337b7
PhilReinking <media@philippreinking.de>
2014-01-21 14:03:29 +0100

    Sticky Posting Actions Fix

--- www/js/jquery.ppnet.js
+++ www/js/jquery.ppnet.js
@@ -1,14 +1,6 @@
 function stickyActionBar() {
-	jQuery('#app_content_area').waypoint(function(direction){
-		if(direction === "down"){
-			jQuery('.app_action_wrapper').addClass('sticky');
-			jQuery('.postings').addClass('sticky');
-		} else {
-			jQuery('.app_action_wrapper').removeClass('sticky');
-			jQuery('.postings').removeClass('sticky');
-		}
-	});
-	return true;
-};
-
+	setTimeout(function(){
+		jQuery('#posting_actions').waypoint('sticky');
+	}, 1500)
+}
 
