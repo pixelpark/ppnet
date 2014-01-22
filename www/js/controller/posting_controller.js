2c142d7cb72327450393d18c7f4aff7a04e0429b
tobiasrotter <info@tobias-rotter.de>
2014-01-22 10:09:04 +0100

    FCTWO-232

--- www/js/controller/posting_controller.js
+++ www/js/controller/posting_controller.js
@@ -19,14 +19,22 @@ app.controller('PostingController', ['$scope', '$routeParams' , function($scope,
 					  continuous: true,
 					  include_docs: true,
 					  onChange:  function(change) {
-					  	 onChange(change);
+					  	switch (change.doc.type) {
+						    case "POST":
+						        $scope.posting_functions.onChangePosting(change);
+						        break;
+						    case "LIKE":
+						        $scope.like_functions.onChangeLike(change);
+						        break;
+						};					  	 
 					  },
 					  complete: function(err, response) {
-					  	 onComplete();
+					  	
 					  }
 	});
 
-	function onChange(change){
+	$scope.posting_functions.onChangePosting = function(change){
+		console.log('$scope.posting_functions.onChangePosting');
 		if(change.deleted){
 			angular.forEach($scope.postings, function(value, key){
 				if(value.id==change.id){
@@ -37,13 +45,10 @@ app.controller('PostingController', ['$scope', '$routeParams' , function($scope,
 			$scope.postings.push(change);
 		}
 		$scope.apply();		
-	}
-	
-	function onComplete(){
-		$scope.posting_functions.showPostings();
-	}
+	};
 	
 	$scope.posting_functions.delete = function(posting) {
+		console.log('$scope.posting_functions.delete');
 		$scope.db.get(posting.id, function(err, results) {
 			$scope.db.remove(results, function(err, results){});
 		});
@@ -51,11 +56,18 @@ app.controller('PostingController', ['$scope', '$routeParams' , function($scope,
 		
 	};
 	
+
+	$scope.posting_functions.isPost = function(doc) { if(doc.type=='POST') {   emit(doc.created, doc);  }};
 	$scope.posting_functions.showPostings = function() {
-		$scope.db.allDocs({include_docs: true, descending: true}, function(err, docs) {
-			$scope.postings=docs.rows;
+		console.log('$scope.posting_functions.showPostings');
+		$scope.db.query({map: $scope.posting_functions.isPost}, {reduce: false}, function(err, response) {
+			$scope.postings=response.rows;
+			angular.forEach($scope.postings, function(value, key){
+				$scope.postings[key].doc=value.value;
+				delete  $scope.postings[key].value;
+			});
 			$scope.apply();
-	    });	
+		});
     };
     
     $scope.posting_functions.orderByCreated = function(posting) {
@@ -75,17 +87,34 @@ app.controller('PostingController', ['$scope', '$routeParams' , function($scope,
 	 $scope.posting_functions.showTimestamp = function(posting) {
 	 	// Generate timestamp for 24hours time difference
 	 	maxTimeDifference = Math.round((new Date()).getTime() / 1000) - 86400;
-	 	if((posting.doc.created/1000) < maxTimeDifference){
+	 	if((posting.created/1000) < maxTimeDifference){
 	 		return true;
 	 	}
 	 	return false;
 	 };
 	 
 	 
+	 
+	 
+	 
 	 $scope.like={};
 	 $scope.likes={};
 	 $scope.like_functions={};
-	 $scope.like_functions.create = function(){
+	 $scope.like_functions.like = function(posting){
+	 	doc={ 
+			created : new Date().getTime(),
+			posting: posting.id,
+			user: {
+				id : $scope.user.getId(),
+				name : $scope.user.getName()
+			},
+			type : 'LIKE'
+		};
+		$scope.db.post(doc, function (err, response) {});
+	 };
+	 
+	 $scope.like_functions.onChangeLike = function(change){
+	 	console.log('$scope.like_functions.onChangeLike');
 	 	
 	 };
 
