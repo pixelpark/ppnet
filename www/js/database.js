082ccc56242b6f0fde1e77a1f6500cb33d884e4e
tobiasrotter <info@tobias-rotter.de>
2014-01-22 12:01:20 +0100

    Merge branch 'master' of https://github.com/pixelpark/ppnet
    Conflicts:
    	www/js/controller/posting_controller.js
    FCTWO-232FCTWO-232

--- www/js/database.js
+++ www/js/database.js
@@ -2,7 +2,15 @@ function Database ($scope) {
 	//new PostingController();
 	
     $scope.db = new PouchDB('ppnet');
-	var remoteCouch = 'http://couchdb.simple-url.com:5984/ppnet';
+
+	//AMAZON
+	var remoteCouch = 'http://107.20.67.201:5984/ppnet';
+	
+	//FI-Ware
+	//var remoteCouch = ' http://130.206.83.238:5984/ppnet';	
+	
+	//var remoteCouch = 'http://couchdb.simple-url.com:5984/ppnet';
+		
 	function sync() {
 		  var opts = {continuous: true};
 		  $scope.db.replicate.from(remoteCouch, opts);
--- www/js/directive/posting.js
+++ www/js/directive/posting.js
@@ -1,8 +1,6 @@
 app.directive('postingformat', function() {
 	
 	function hashtag(text){
-		
-		
 		text= text.replace(/#(\S*)/g,'<a href="#/hashtag/$1" class="posting_hashtag">#$1</a>');
 		return text;
 	}
@@ -10,11 +8,12 @@ app.directive('postingformat', function() {
 	return {
 		restrict: 'AE',
 		link: function(scope, element, attrs) {
-			scope.posting.doc.msg = hashtag(scope.posting.doc.msg);
+			scope.posting.doc.msg_formatted = scope.posting.doc.msg;
+			hashtag(scope.posting.doc.msg_formatted);
 		},
 		scope: {
 			posting: '=postingformat'
 		},
-		template: '<div class="posting_msg" ng-bind-html="posting.doc.msg"></div>'
+		template: '<div class="posting_msg" ng-bind-html="posting.doc.msg_formatted"></div>'
 	};
 });
