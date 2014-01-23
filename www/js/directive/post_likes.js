cec926074bb3323ec933e8201b921fd36da45c51
PhilReinking <media@philippreinking.de>
2014-01-23 16:51:38 +0100

    FTCWO-232 Like Directive hinzugefügt

---
+++ www/js/directive/post_likes.js
@@ -0,0 +1,17 @@
+app.directive('ppnetPostLikes', function(){
+	var countLikes = function(allLikes){
+		if(Array.isArray(allLikes))
+			return allLikes.length;
+	};
+
+	return {
+		restrict: 'E',
+		scope: {
+			likes: '=likes'
+		},
+		link: function(scope, element, attrs){
+			scope.counted_likes = countLikes(scope.likes);
+		},
+		template: '<span ng-show="counted_likes">{{counted_likes}} <i class="fa fa-thumbs-up"></i></span>'
+	};
+});
