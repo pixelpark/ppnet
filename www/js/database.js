00e3acda2e9357ebb14776fedf0b7bb55c599e2f
PhilReinking <media@philippreinking.de>
2014-01-28 09:29:22 +0100

    database.js initial replication + filter settings

--- www/js/database.js
+++ www/js/database.js
@@ -1,21 +1,65 @@
 function Database ($scope) {   
 	//new PostingController();
 	
+    //$scope.db = new PouchDB('ppnet', {auto_compaction: true});
     $scope.db = new PouchDB('ppnet');
 
 	//AMAZON
 	var remoteCouch = 'http://107.20.67.201:5984/ppnet';
 	
 	//FI-Ware
-	//var remoteCouch = ' http://130.206.83.238:5984/ppnet';	
+	//var remoteCouch = 'http://130.206.83.238:5984/ppnet';	
 	
 	//var remoteCouch = 'http://couchdb.simple-url.com:5984/ppnet';
-		
-	function sync() {
-		  var opts = {continuous: true};
-		  $scope.db.replicate.from(remoteCouch, opts);
-		  $scope.db.replicate.to(remoteCouch, opts);
+
+
+	// Function for continuous sync
+	var sync = function(){
+		var opts = {
+			continuous: true
+		};
+		//console.log('continuous Replicate FROM');
+		$scope.db.replicate.from(remoteCouch, opts);
+		//console.log('continuous Replicate TO');
+		$scope.db.replicate.to(remoteCouch, opts);
 	};
-	if (remoteCouch){sync();}
+
+	// Inital Replication from remote to local (data-subset: only POSTs not older than 24hours)
+	var initialReplicateFrom = function(){
+		var args = {
+			continuous: false,
+			complete: function(){
+				initialReplicateTo();
+			},
+			filter: function(doc, req) {
+				var oldest_timestamp = new Date().getTime() - (86400*1000);
+				if (doc.created > oldest_timestamp && doc.type && doc.type == "POST") {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		};
+		//console.log('Initial Replicate FROM');
+		$scope.db.replicate.from(remoteCouch, args);
+	};
+
+	// Inital Replication from local to remote (no data-subset)
+	// After Replication call the continious sync function
+	var initialReplicateTo = function(){
+		args = {
+			complete: function(){
+				sync();
+				// TODO Call the db.changes method to watch changes
+			}
+		};
+		//console.log('Initial Replicate TO');
+		$scope.db.replicate.to(remoteCouch, args);
+	};
+
+	if(remoteCouch){
+		initialReplicateFrom();
+	}
+	//if (remoteCouch){sync();}
 }
 
