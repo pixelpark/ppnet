7be6b665a96c9da6c91db364ee19bf16d13abbfb
Tobias Rotter <info@tobias-rotter.de>
2014-03-07 16:31:48 +0100

    Bug-Fixing

--- www/js/database.js
+++ www/js/database.js
@@ -1,22 +1,25 @@
-function Database ($scope) {   
+function Database($scope) {
 	//new PostingController();
-	PouchDB.DEBUG=true;
-	var db='ppnet_suelz';
-	
-    $scope.db = new PouchDB(db, {debug:true,auto_compaction: true});
-    //$scope.db = new PouchDB('ppnet');
+	PouchDB.DEBUG = true;
+	var db = 'ppnet_suelz';
+
+	$scope.db = new PouchDB(db, {
+		debug: true,
+		auto_compaction: true
+	});
+	//$scope.db = new PouchDB('ppnet');
 
 	//AMAZON
 	//$scope.remoteCouch = 'http://107.20.67.201:5984/'+db;
-	
+
 	//FI-Ware
 	//$scope.remoteCouch = 'http://130.206.83.238:5984/'+db';	
-	
-	$scope.remoteCouch = 'http://couchdb.simple-url.com:5984/'+db;
-	
+
+	$scope.remoteCouch = 'http://couchdb.simple-url.com:5984/' + db;
+
 	// Function for continuous sync
-	
-	$scope.sync = function(){
+
+	$scope.sync = function() {
 		console.log('sync');
 		var opts = {
 			continuous: true,
@@ -24,39 +27,38 @@ function Database ($scope) {
 		};
 		$scope.db.replicate.from($scope.remoteCouch, opts);
 	};
-	
+
 
 	// Inital Replication from remote to local (data-subset: only POSTs not older than 24hours)
-	var initialReplicateFrom = function(time){
-		console.log('START: initialReplicateFrom ('+time+')');
+	var initialReplicateFrom = function(time) {
+		console.log('START: initialReplicateFrom (' + time + ')');
 		var args = {
-			complete: function(){
-				console.log('FINISH: initialReplicateFrom ('+time+') args');
+			complete: function() {
+				console.log('FINISH: initialReplicateFrom (' + time + ') args');
 				//initialReplicateFromOld(time);
 			},
-			onChange: function(change){},
+			onChange: function(change) {},
 			filter: function(doc, req) {
-				var oldest_timestamp = time - (86400*1000);
-				
+				var oldest_timestamp = time - (86400 * 1000);
+
 				//if (doc.created > oldest_timestamp && doc.type && doc.type == "POST") {
 				if (doc.created > oldest_timestamp) {
 					return true;
 				} else {
 					return false;
 				}
-				
+
 			}
 		};
 		$scope.db.replicate.from($scope.remoteCouch, args);
 	};
-	
-	if($scope.remoteCouch){
+
+	if ($scope.remoteCouch) {
 		$scope.sync();
 		initialReplicateFrom(new Date().getTime());
 	}
-	Offline.on('up', function(){
+	Offline.on('up', function() {
 		$scope.sync();
 		initialReplicateFrom(new Date().getTime());
-	},'');
+	}, '');
 }
-
--- www/js/routes.js
+++ www/js/routes.js
@@ -1,23 +1,24 @@
-app.config(['$routeProvider', function($routeProvider) {
-		
-	$routeProvider.when('/posting', {
-	  controller: 'PostingController',
+app.config(['$routeProvider',
+  function($routeProvider) {
+
+    $routeProvider.when('/posting', {
+      controller: 'PostingController',
       templateUrl: 'html/views/posting.html'
     });
-    
-	$routeProvider.when('/hashtag/:hashtag', {
-		controller: 'HashtagController',
+
+    $routeProvider.when('/hashtag/:hashtag', {
+      controller: 'HashtagController',
       templateUrl: 'html/views/hashtag.html'
     });
-    
+
     $routeProvider.when('/hashtag', {
       templateUrl: 'html/views/hashtag.html'
     });
-     
-	$routeProvider.when('/login', {
+
+    $routeProvider.when('/login', {
       templateUrl: 'html/views/login.html'
     });
-    
+
     $routeProvider.when('/report', {
       controller: 'ReportController',
       templateUrl: 'html/views/report.html'
@@ -25,23 +26,25 @@ app.config(['$routeProvider', function($routeProvider) {
 
     $routeProvider.when('/user/:task', {
       controller: 'UserController',
-      template : ''
+      template: ''
     });
-    
+
     $routeProvider.when('/view/:view', {
       controller: 'ViewController',
-      templateUrl:function(params) {return "html/views/" + params.view +'.html';}
+      templateUrl: function(params) {
+        return "html/views/" + params.view + '.html';
+      }
     });
 
     $routeProvider.when('/auth', {
-        controller: 'AuthController',
-        templateUrl: 'html/views/auth.html'
+      controller: 'AuthController',
+      templateUrl: 'html/views/auth.html'
     });
 
     $routeProvider.otherwise({
-    	controller: 'PostingController',
+      controller: 'PostingController',
       templateUrl: 'html/views/posting.html'
     });
-    
-}]); 
 
+  }
+]);
