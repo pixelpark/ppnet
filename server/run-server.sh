480796b564a6e56a738fa3bc923e92def9642c98
teawithfruit <teawithfruit@gmail.com>
2015-03-23 17:39:31 +0100

    replace only if env is set

--- server/run-server.sh
+++ server/run-server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # A script to change the remote url before starting the ppnet server
 
-sed -i 's|http://couchdb.simple-url.com:5984/|http://'${PPNET_SERVER_URL}'/|g' /home/ppnet/www/config.json
+if [ ! -z "$PPNET_SERVER_URL"  ] && [ "$PPNET_SERVER_URL" != "http://:8000" ]; then
+  sed -i 's|http://couchdb.simple-url.com:5984/|http://'${PPNET_SERVER_URL}'/|g' /home/ppnet/www/config.json
+fi
 
 /usr/local/bin/node server.js
